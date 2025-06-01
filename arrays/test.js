10c09e1b57e9e6e446e0f59c88a9c68c3075d66d
vishwa-radhya <vishuvishwa735@gmail.com>
2025-06-01 22:53:08 +0530

    add

--- arrays/test.js
+++ arrays/test.js
@@ -99,27 +99,136 @@ function product_of_array_except_itself(nums){
 // console.log(product_of_array_except_itself([1,2,3,4]))
 // console.log(product_of_array_except_itself([-1,1,0,-3,3]))
 function first_missing_positive(nums){
-    if(nums.length===1 && nums[0]===0) return 1
-    let allNeg = nums.filter(e=>e<=0).length
-    if(allNeg===nums.length) return 1
-    nums=[...new Set(nums.sort((a,b)=>a-b).filter(e=>e>0))]
-    let k=0;
-    let length=nums.length-1
-    for(let i=1;i<=nums[length]+1;i++){
-        if(i<nums[k]){
-            return i
-        }
-        if(i!==nums[k]){o
-            return i
-        }
-        k++
-    }
+    // if(nums.length===1 && nums[0]===0) return 1
+    // let allNeg = nums.filter(e=>e<=0).length
+    // if(allNeg===nums.length) return 1
+    // nums=[...new Set(nums.sort((a,b)=>a-b).filter(e=>e>0))]
+    // let k=0;
+    // let length=nums.length-1
+    // for(let i=1;i<=nums[length]+1;i++){
+    //     if(i<nums[k]){
+    //         return i
+    //     }
+    //     if(i!==nums[k]){
+    //         return i
+    //     }
+    //     k++
+    // }
     // solved but took a lot of time nearly 70ms+ in nodejs leetcode env
     // optims
-    
+    // brute force appraoch which takes O(n^2)
+    // let len = nums.length;
+    // for(let i=1;i<=len+1;i++){
+    //     let found = false
+    //     for(let num of nums){
+    //         if(num===i){
+    //             found =true
+    //             break
+    //         }
+    //     }
+    //     if(!found){
+    //         return i
+    //     }
+    // }
+    // return -1
+    // fails for large test cases like 1 to 100000
+    // coz we may check the duplicates also it takes O(n^2)
+    // use hashset which has O(1) access to make the tc to O(n)
+    // let set = new Set()
+    // let length=nums.length
+    // for(let i=0;i<length;i++){
+    //     set.add(nums[i])
+    // }
+    // for(let i=1;i<=length+1;i++){
+    //     if(!set.has(i)){
+    //         return i;
+    //     }
+    // }
+    // return -1
+    // solves but still has the best approaches
+    // using cyclic sort assuming the range is between 1 to n inclusive
+    let len = nums.length
+    for(let i=0;i<len;i++){
+        while(nums[i]>0 && nums[i]<=len && nums[nums[i]-1] !== nums[i]){
+            let temp = nums[nums[i]-1]
+            nums[nums[i]-1]=nums[i]
+            nums[i]=temp
+        }
+    }
+    for(let i=0;i<len;i++){
+        if(nums[i]!=i+1){
+            return i+1
+        }
+    }
+    return len+1
 }
 // console.log(first_missing_positive([1,2,0]))
 // console.log(first_missing_positive([3,4,-1,1]))
 // console.log(first_missing_positive([7,8,9,11,12]))
 // console.log(first_missing_positive([1,2,0]))
-console.log(first_missing_positive([0,2,2,1,1]))
+// console.log(first_missing_positive([0,2,2,1,1]))
+// console.log(first_missing_positive([-1,-2,-3]))
+// console.log(first_missing_positive([-1,0]))
+
+function num_of_zero_filled_sub_arrays(nums){
+    // let res=0
+    // let i=0
+    // let len = nums.length
+    // while(i<len){
+    //     let countContinuosZeros=0
+    //     let j=i
+    //     while(j<len){
+    //         if(nums[j]!==0){
+    //             break
+    //         }
+    //         j++
+    //     }
+    //     countContinuosZeros=j-i
+    //     if(countContinuosZeros){
+    //         i+=countContinuosZeros
+    //     }else{
+    //         i++
+    //     }
+    //     if(countContinuosZeros){
+    //         // for(let k=countContinuosZeros;k>=1;k--){
+    //         //     res+=k;
+    //         // }
+    //         // avoiding loop through with math formula num of sub zeroes is (n*(n+1))/2
+    //         // as we saw earlier [0,0] = 2+1
+    //         // as we saw earlier [0,0,0] = 3+2+1
+    //         // as we saw earlier [0,0,0,0] = 4+3+2+1
+    //         // it is sum of n natural numbers
+    //         // so,
+    //         res+=((Math.floor(countContinuosZeros*(countContinuosZeros+1)))/2)
+    //     }
+    // }
+    // return res
+    // success beat with tc 5ms but it has (not takes) O(n^2) tc
+    // optimal one with O(n) tc
+    let count=0,res=0
+    for(let num of nums){
+        if(num===0){
+            count++
+            res+=count
+        }else{
+            count=0
+        }
+    }
+    return res
+    // just twaeking code structure gives less tc
+    // let count=0
+    // let res=0
+    // for(let i=0;i<nums.length;i++){
+    //     if(nums[i]==0){
+    //         count=count+1
+    //         res=res+count
+    //     }else{
+    //         count=0
+    //     }
+    // }
+    // return res
+    // like so whatever
+}
+// console.log(num_of_zero_filled_sub_arrays([1,3,0,0,2,0,0,4]))
+// console.log(num_of_zero_filled_sub_arrays([0,0,0,2,0,0]))
+// console.log(num_of_zero_filled_sub_arrays([2,10,2019]))
---
+++ leetcode_dsa_mastery_plan.md
@@ -0,0 +1,26 @@
+# 🧠 LeetCode DSA Mastery Plan (Notion Format)
+
+| ✅ | 🔢 Order | 🧩 Topic                   | 🎯 Key Concepts                                | 📌 Must-Do Problems                                                                                   |
+|----|----------|----------------------------|------------------------------------------------|--------------------------------------------------------------------------------------------------------|
+| ⬜ | 1        | Arrays (Finish)           | Iteration, Prefix, Sorting, Subarrays         | First Missing Positive, Increasing Triplet Subsequence, Number of Zero-Filled Subarrays              |
+| ⬜ | 2        | Strings (Finish)          | Tokenization, Reversal, In-Place Ops          | Reverse Words in a String, Guess the Word                                                             |
+| ⬜ | 3        | Hash Tables               | Frequency Maps, Sets, Anagrams                | Group Anagrams, Longest Consecutive Sequence, Ransom Note, Reorganize String                          |
+| ⬜ | 4        | Two Pointers (Finish)     | Sorted + In-Place + Shrinking Window          | 3Sum, Trapping Rain Water                                                                             |
+| ⬜ | 5        | Prefix Sum                | Range Queries, Subarray Sums                  | Range Sum Query, Subarray Sum Equals K, Contiguous Array                                              |
+| ⬜ | 6        | Sliding Window            | Fixed/Dynamic Window, Min/Max Conditions      | Permutation in String, Min Window Substring, Sliding Window Maximum                                   |
+| ⬜ | 7        | Kadane's Algorithm        | Max Subarray/Product, Prefix Resetting        | Max Subarray, Max Product Subarray, Best Sightseeing Pair                                             |
+| ⬜ | 8        | Recursion                 | Base Case, Subproblems, Stack Frame           | Decode String, Pow(x, n), Merge Two Sorted Lists                                                      |
+| ⬜ | 9        | Divide & Conquer          | Merge Sort, Inversion Count                   | Sort List, Reverse Pairs, Kth Largest in Array                                                        |
+| ⬜ | 10       | Binary Search             | Low/High Shrink, Mid Check                    | Find Peak Element, Search Rotated Array, Find Minimum in Rotated Array                                |
+| ⬜ | 11       | Linked List               | Reversal, Cycle, Two Pointer                  | Palindrome LL, Add Two Numbers, Remove Nth Node                                                       |
+| ⬜ | 12       | Stack                     | LIFO, Expression Eval, Matching               | Valid Parentheses, Min Stack, Remove Stars, Basic Calculator                                          |
+| ⬜ | 13       | Queue / Monotonic Queue   | FIFO, Max in Window                           | Sliding Window Max, Time to Buy Tickets, Longest Subarray Absolute Diff                               |
+| ⬜ | 14       | Tree Traversals           | DFS/BFS, Recursion                            | Level Order, Inorder, LCA, Construct Tree from Pre+Inorder                                            |
+| ⬜ | 15       | Backtracking              | All Possible Paths, Combinations              | Generate Parentheses, Permutations, Subsets, Combination Sum                                          |
+| ⬜ | 16       | DFS + BFS (Graphs)        | Matrix DFS/BFS, Components                    | Number of Islands, Clone Graph, 01 Matrix, Rotting Oranges, Open Lock                                 |
+| ⬜ | 17       | Greedy                    | Best-Local Choice                             | Jump Game II, Gas Station, Minimum Add to Make Valid Parentheses                                      |
+| ⬜ | 18       | Heap / Priority Queue     | MinHeap/MaxHeap                               | Top K Frequent Elements, Kth Largest, Task Scheduler                                                  |
+| ⬜ | 19       | Monotonic Stack           | Next Greater Element, Histograms              | Daily Temperatures, Largest Rectangle, 132 Pattern                                                    |
+| ⬜ | 20       | Intervals                 | Merging, Sorting, Overlaps                    | Merge Intervals, Insert Interval, Non-overlapping Intervals, My Calendar I                            |
+| ⬜ | 21       | Trie                      | Prefix Tree, Word Filtering                   | Implement Trie, Search Suggestions, Longest Word in Dictionary                                        |
+| ⬜ | 22       | Design                    | Object-Oriented, Caching                      | LRU Cache, Browser History, Insert Delete GetRandom                                                   |
