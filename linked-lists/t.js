ea0b13385a58c7facf97d526c039d84324370fbb
vishwa-radhya <vishuvishwa735@gmail.com>
2025-07-31 20:42:01 +0530

    on linked lists

--- linked-lists/t.js
+++ linked-lists/t.js
@@ -396,43 +396,297 @@ function remove_nth_node_from_end_of_list(head,n){
 // console.log(remove_nth_node_from_end_of_list(convertToLeetCodeList(list1.head),1))
 
 function add_two_numbers(l1,l2){
-    let l1Arr=[]
-    let l2Arr=[]
-    let c1 =l1
-    let c2 =l2
-    while(c1){
-        l1Arr.push(c1.val)
-        c1=c1.next
-    }
-    while(c2){
-        l2Arr.push(c2.val)
-        c2=c2.next
-    }
-    let sum = Number(l1Arr.reverse().join(''))+Number(l2Arr.reverse().join(''))
-    if(sum==0){
-        let res = new ListNode()
-        return res
-    }
-    let res = new ListNode()
-    let c= res
-    while(sum>0){
-        c.next=new ListNode(sum%10)
-        c=c.next
-        sum=Math.floor(sum/10)
-    }
-    return res.next
+    // let l1Arr=[]
+    // let l2Arr=[]
+    // let c1 =l1
+    // let c2 =l2
+    // while(c1){
+    //     l1Arr.push(c1.val)
+    //     c1=c1.next
+    // }
+    // while(c2){
+    //     l2Arr.push(c2.val)
+    //     c2=c2.next
+    // }
+    // let sum = Number(l1Arr.reverse().join(''))+Number(l2Arr.reverse().join(''))
+    // if(sum==0){
+    //     let res = new ListNode()
+    //     return res
+    // }
+    // let res = new ListNode()
+    // let c= res
+    // while(sum>0){
+    //     c.next=new ListNode(sum%10)
+    //     c=c.next
+    //     sum=Math.floor(sum/10)
+    // }
+    // return res.next
     //wrong answer at 1566/1569 due to large number addition
     //should follow add from reverse then chain the value from front handling nodeval+nodeval >= 10
-    
+    let res = new ListNode()
+    let resCurr = res
+    let remainingVal=0
+    while(l1 && l2){
+        let sum = l1.val+l2.val
+        if(remainingVal>0){
+            sum+=remainingVal
+            remainingVal=0
+        }
+        if(sum>=10){
+            let t = sum
+            sum = sum%10
+            remainingVal = Math.floor(t/10)
+        }
+        resCurr.next= new ListNode(sum)
+        resCurr=resCurr.next
+        l1=l1.next
+        l2=l2.next
+    }
+    if(remainingVal==0 && (l1 || l2)){
+        resCurr.next = l1 || l2
+    }
+    else if(!l1 && !l2 && remainingVal>0){
+        resCurr.next = new ListNode(remainingVal)
+        resCurr=resCurr.next
+    }
+    else if(l1 && remainingVal>0){
+        while(l1){
+            let sum = l1.val+remainingVal
+            remainingVal=0
+            if(sum>=10){
+                let t=sum
+                sum=sum%10
+                remainingVal=Math.floor(t/10)
+            }
+            resCurr.next = new ListNode(sum)
+            resCurr=resCurr.next
+            l1=l1.next
+        }
+        if(remainingVal){
+            resCurr.next = new ListNode(remainingVal)
+        }
+    }else if(l2 && remainingVal>0){
+        while(l2){
+            let sum = l2.val+remainingVal
+            remainingVal=0
+            if(sum>=10){
+                let t=sum
+                sum=sum%10
+                remainingVal=Math.floor(t/10)
+            }
+            resCurr.next = new ListNode(sum)
+            resCurr=resCurr.next
+            l2=l2.next
+        }
+        if(remainingVal){
+            resCurr.next = new ListNode(remainingVal)
+        }
+    }
+    return res.next
+    //3ms with 57.57% beat
+    //the 0ms sol with precomputed edge cases
+    // let q = 0;
+    // let sentinel = new ListNode();
+    // let temp = sentinel;
+    // while (l1 || l2) {
+    //     let v1 = l1 ? l1.val : 0;
+    //     let v2 = l2 ? l2.val : 0;
+    //     let sum = v1 + v2 + q;
+    //     temp.next = new ListNode(sum % 10);
+    //     temp = temp.next;
+    //     q = Math.floor(sum / 10);
+    //     l1 = l1 ? l1.next : null;
+    //     l2 = l2 ? l2.next : null;
+    // }
+    // if (q > 0) temp.next = new ListNode(q);
+    // return sentinel.next;
 }
 // l1 = [2,4,3], l2 = [5,6,4] [7,0,8] 342 + 465 = 807
-const list1 = new LinkedList();
+// l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] [8,9,9,9,0,0,0,1]
+// const list1 = new LinkedList();
+// list1.append(9)
+// list1.append(9)
+// list1.append(9)
+// list1.append(9)
+// list1.append(9)
+// list1.append(9)
+// list1.append(9)
+// list1.append(1)
+// const list2 = new LinkedList();
+// list2.append(9)
+// list2.append(9)
+// list2.append(9)
+// list2.append(9)
+// console.log(add_two_numbers(convertToLeetCodeList(list1.head),convertToLeetCodeList(list2.head)))
+
+//design linked list
+
+function rotate_list(head,k){
+    // let c = head
+    // let arr=[]
+    // while(c){
+    //     arr.push(c.val)
+    //     c=c.next
+    // }
+    // let arrLen = arr.length
+    // k = k%arrLen
+    // let l=0
+    // let r=arrLen-1
+    // while(l<r){
+    //     let t = arr[l]
+    //     arr[l]=arr[r]
+    //     arr[r]=t
+    //     l++
+    //     r--
+    // }
+    // l=0
+    // r=k-1
+    // while(l<r){
+    //     let t = arr[l]
+    //     arr[l]=arr[r]
+    //     arr[r]=t
+    //     l++
+    //     r--
+    // }
+    // l=k
+    // r=arrLen-1
+    // while(l<r){
+    //     let t = arr[l]
+    //     arr[l]=arr[r]
+    //     arr[r]=t
+    //     l++
+    //     r--
+    // }
+    // let res = new ListNode()
+    // c=res
+    // for(let i=0;i<arrLen;i++){
+    //     c.next = new ListNode(arr[i])
+    //     c=c.next
+    // }
+    // return res.next
+    //1ms with 24.20% beat (used array)
+    /***************** */
+    // if(!head || !head.next) return head
+    // let listLen =0
+    // let c = head
+    // while(c){
+    //     listLen++
+    //     c=c.next
+    // }
+    // k=k%listLen
+    // let s = head;
+    // let f = head;
+    // for(let i=0;i<k;i++){ 
+    //     f=f.next;
+    // }
+    // while(f.next){
+    //     s = s.next;
+    //     f=f.next;
+    // }
+    // f.next = head;
+    // let newNode = s.next;
+    // s.next = null;
+    // return newNode;
+    /**************************** */
+    if(!head || !head.next) return head
+    let len=0,c=head
+    while(c){
+        len++
+        c=c.next
+    }
+    k=k%len
+    if(k==0) return head
+    function reverseList(head){
+        let prev=null
+        while(head){
+            let next = head.next
+            head.next=prev
+            prev=head
+            head=next
+        }
+        return prev
+    }
+    head = reverseList(head)
+    let first=head,prev=null
+    let i=0
+    while(i<k && first){
+        prev=first
+        first=first.next
+        i++
+    }
+    prev.next=null
+    let firstPart = reverseList(head)
+    let secondPart = reverseList(first)
+    let tail = firstPart
+    while(tail.next) tail=tail.next
+    tail.next=secondPart
+    return firstPart
+    //0ms tc with 100% upbeat
+}
+let list1 = new LinkedList()
+// list1.append(1)
 // list1.append(2)
-// list1.append(4)
 // list1.append(3)
-list1.append(1)
-const list2 = new LinkedList();
-// list2.append(5)
-// list2.append(6)
-list2.append(0)
-console.log(add_two_numbers(convertToLeetCodeList(list1.head),convertToLeetCodeList(list2.head)))
+// list1.append(4)
+// list1.append(5)
+// console.log(rotate_list(convertToLeetCodeList(list1.head),2))
+
+// function ListNode(val, next) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.next = (next===undefined ? null : next)
+// }
+
+// var MyLinkedList = function() {
+    
+// };
+
+// /** 
+//  * @param {number} index
+//  * @return {number}
+//  */
+// MyLinkedList.prototype.get = function(index) {
+    
+// };
+
+// /** 
+//  * @param {number} val
+//  * @return {void}
+//  */
+// MyLinkedList.prototype.addAtHead = function(val) {
+    
+// };
+
+// /** 
+//  * @param {number} val
+//  * @return {void}
+//  */
+// MyLinkedList.prototype.addAtTail = function(val) {
+    
+// };
+
+// /** 
+//  * @param {number} index 
+//  * @param {number} val
+//  * @return {void}
+//  */
+// MyLinkedList.prototype.addAtIndex = function(index, val) {
+    
+// };
+
+// /** 
+//  * @param {number} index
+//  * @return {void}
+//  */
+// MyLinkedList.prototype.deleteAtIndex = function(index) {
+    
+// };
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
